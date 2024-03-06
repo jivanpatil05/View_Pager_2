6252331f64f71bdd71addc60d75388172981c571
jivan <g1patil0509@gmail.com>
2024-03-06 17:26:28 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/view_pager_2/SliderItem.java
@@ -0,0 +1,11 @@
+package com.example.view_pager_2;
+
+    public class SliderItem {
+
+        private int image;
+
+        SliderItem(int image) {this.image = image;
+        }
+        public int getImage() { return image;
+        }
+}
